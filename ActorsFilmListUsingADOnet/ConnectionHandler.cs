0ef576891e3c52d58b7db27338042ca97f33c01d
jonat <jonat@KALLIOPE>
2025-04-13 21:13:01 +0200

    improved readability and fixed some potential resource leaks

--- ActorsFilmListUsingADOnet/ConnectionHandler.cs
+++ ActorsFilmListUsingADOnet/ConnectionHandler.cs
@@ -27,10 +27,15 @@ namespace ActorsFilmListUsingADOnet
             using (SqlConnection connection = new SqlConnection(ConnectionString))
             {
                 connection.Open();
-                SqlCommand command = new SqlCommand("SELECT first_name, last_name " +
-                    "FROM actor", connection);
-                var receiver = command.ExecuteReader();
-                while (receiver.Read()) Console.WriteLine($"{receiver[0]} {receiver[1]}");
+                using (SqlCommand command = new SqlCommand("SELECT first_name, last_name " +
+                    "FROM actor", connection))
+                using (var receiver = command.ExecuteReader())
+                {
+                    while (receiver.Read())
+                    {
+                        Console.WriteLine($"{receiver["first_name"]} {receiver["last_name"]}");
+                    }
+                }
             }
         }
         public void ListMovies(string firstName, string lastName)
@@ -38,15 +43,22 @@ namespace ActorsFilmListUsingADOnet
             using (SqlConnection connection = new SqlConnection(ConnectionString))
             {
                 connection.Open();
-                SqlCommand command = new SqlCommand("SELECT film.title " +
+                using (SqlCommand command = new SqlCommand("SELECT film.title " +
                     "FROM actor " +
                     "INNER JOIN film_actor on film_actor.actor_id=actor.actor_id " +
                     "INNER JOIN film on film.film_id=film_actor.film_id " +
-                    "WHERE first_name = @firstName and last_name = @lastName", connection);
-                command.Parameters.AddWithValue("@firstName", firstName);
-                command.Parameters.AddWithValue("@lastName", lastName);
-                var receiver = command.ExecuteReader();
-                while (receiver.Read()) Console.WriteLine(receiver[0]);
+                    "WHERE first_name = @firstName and last_name = @lastName", connection))
+                {
+                    command.Parameters.AddWithValue("@firstName", firstName);
+                    command.Parameters.AddWithValue("@lastName", lastName);
+                    using (var receiver = command.ExecuteReader())
+                    {
+                        while (receiver.Read())
+                        {
+                            Console.WriteLine(receiver["title"]);
+                        }
+                    }
+                }
             }
         }
     }
